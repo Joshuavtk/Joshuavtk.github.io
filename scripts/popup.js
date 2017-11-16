4a2e5c263d3c8dd254f9e308cca725ea3212b942
Joshua <joshuavtk@hotmail.com>
2017-11-16 17:28:19 +0100

    Change js code to english
    Some variable names were changed to the english language and i added ::selection

--- scripts/popup.js
+++ scripts/popup.js
@@ -6,54 +6,54 @@ exitButton.innerHTML = '&Cross;';
 exitButton.className = 'exit-button';
 
 // Initialise popup functions
-const modaal = (function () {
-    const modaalWindow = document.createElement('div');
-    modaalWindow.className = 'modaal-window';
-    modaalWindow.addEventListener('click', function (e) {
+const popup = (function () {
+    const popupWindow = document.createElement('div');
+    popupWindow.className = 'popup-window';
+    popupWindow.addEventListener('click', function (e) {
         e.stopPropagation();
     });
-    const modaalContent = document.createElement('div');
-    modaalContent.className = 'modaal-content';
+    const popupContent = document.createElement('div');
+    popupContent.className = 'popup-content';
 
     return {
-        centreren: function () {
-            let top = Math.max((grayBackground.offsetHeight - modaalWindow.offsetHeight) / 2, 0);
-            let left = Math.max((grayBackground.offsetWidth - modaalWindow.offsetWidth) / 2, 0);
-            modaalWindow.style.top = top + 'px';
-            modaalWindow.style.left = left + 'px';
+        center: function () {
+            let top = Math.max((grayBackground.offsetHeight - popupWindow.offsetHeight) / 2, 0);
+            let left = Math.max((grayBackground.offsetWidth - popupWindow.offsetWidth) / 2, 0);
+            popupWindow.style.top = top + 'px';
+            popupWindow.style.left = left + 'px';
         },
         open: function (parameter) {
-            modaalContent.appendChild(parameter);
-            modaalWindow.appendChild(modaalContent);
-            modaalWindow.appendChild(exitButton);
+            popupContent.appendChild(parameter);
+            popupWindow.appendChild(popupContent);
+            popupWindow.appendChild(exitButton);
 
             grayBackground.className = 'gray-background';
-            grayBackground.appendChild(modaalWindow);
-            modaal.centreren();
-            window.addEventListener('resize', modaal.centreren(), true);
+            grayBackground.appendChild(popupWindow);
+            popup.center();
+            window.addEventListener('resize', popup.center(), true);
         },
         close: function () {
-            modaalContent.innerHTML = '';
-            modaalWindow.innerHTML = '';
-            grayBackground.removeChild(modaalWindow);
+            popupContent.innerHTML = '';
+            popupWindow.innerHTML = '';
+            grayBackground.removeChild(popupWindow);
             grayBackground.className = '';
         }
     }
 }());
 
 // Event listeners
-grayBackground.addEventListener('click', modaal.close);
-exitButton.addEventListener('click', modaal.close);
+grayBackground.addEventListener('click', popup.close);
+exitButton.addEventListener('click', popup.close);
 
 // Array with popup articles
 const articles = ['fanstille', 'maanroosvis', 'fotogallerij', 'hallowereld'];
-let knoppen = [];
+let buttons = [];
 let contents = [];
 for (let i = 0; i < articles.length; i++) {
     contents[i] = document.getElementById('content_' + articles[i]);
-    knoppen[i] = document.getElementById('article_' + articles[i]);
+    buttons[i] = document.getElementById('article_' + articles[i]);
     contents[i].parentNode.removeChild(contents[i]);
-    knoppen[i].addEventListener('click', function () {
-        modaal.open(contents[i]);
+    buttons[i].addEventListener('click', function () {
+        popup.open(contents[i]);
     });
 }

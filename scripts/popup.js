60a1016f6350b0f54ee6540f2533e276a2299de2
Joshua <joshuavtk@hotmail.com>
2018-01-18 21:28:08 +0100

    Add button click effect
    and roboto

--- scripts/popup.js
+++ scripts/popup.js
@@ -46,7 +46,7 @@ grayBackground.addEventListener('click', popup.close);
 exitButton.addEventListener('click', popup.close);
 
 // Array with popup articles
-const articles = ['chessgame', 'fanstille', 'maanroosvis', 'rekenspel', 'fotogallerij', 'hallowereld'];
+const articles = [ 'chessgame', 'fanstille', 'maanroosvis', 'rekenspel', 'fotogallerij', 'hallowereld'];
 let buttons = [];
 let contents = [];
 for (let i = 0; i < articles.length; i++) {
